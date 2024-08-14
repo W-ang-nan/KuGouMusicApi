ef3a299105634fddd8d4f3fc3b352cb3b0730f1b
wangnan <wangnan_yx@163.com>
2024-08-14 10:47:28 +0800

    修改cors

--- server.js
+++ server.js
@@ -68,9 +68,10 @@ async function consturctServer(moduleDefs) {
     if (req.path !== '/' && !req.path.includes('.')) {
       res.set({
         'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Origin': req.headers.origin || '*',
+        // 'Access-Control-Allow-Origin': req.headers.origin || '*',
+        'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
-        'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
         'Content-Type': 'application/json; charset=utf-8',
       });
     }
