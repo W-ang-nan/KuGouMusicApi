3d47dc58d6e0a730242484f2d84bb8bbed287ed0
wangnan <wangnan_yx@163.com>
2024-08-14 10:57:24 +0800

    更改serve

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ async function getModulesDefinitions(modulesPath, specificRoute, doRequire = tru
  */
 async function consturctServer(moduleDefs) {
   const app = express();
-  // const { CORS_ALLOW_ORIGIN } = process.env;
+  const { CORS_ALLOW_ORIGIN } = process.env;
   app.set('trust proxy', true);
 
   /**
@@ -68,10 +68,9 @@ async function consturctServer(moduleDefs) {
     if (req.path !== '/' && !req.path.includes('.')) {
       res.set({
         'Access-Control-Allow-Credentials': true,
-        // 'Access-Control-Allow-Origin': req.headers.origin || '*',
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': CORS_ALLOW_ORIGIN || req.headers.origin || '*',
         'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+        'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
         'Content-Type': 'application/json; charset=utf-8',
       });
     }
